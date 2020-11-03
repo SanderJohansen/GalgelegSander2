9aad5ea5ec027e9aed16bd7d182c1b3aef373033
SanderJohansen <55443844+SanderJohansen@users.noreply.github.com>
2020-11-03 21:17:42 +0100

    Start af implementation af preferencemanager og listView

--- app/src/main/java/com/example/galgeleg_sander/WinnerScreen.java
+++ app/src/main/java/com/example/galgeleg_sander/WinnerScreen.java
@@ -3,12 +3,17 @@ package com.example.galgeleg_sander;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class WinnerScreen extends AppCompatActivity {
+    SharedPreferences prefs;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,16 +21,23 @@ public class WinnerScreen extends AppCompatActivity {
         setContentView(R.layout.winner_screen);
 
         Intent intent = getIntent();
-        String text = intent.getStringExtra(MainActivity.EXTRA_TEXT);
-        int number = intent.getIntExtra(MainActivity.EXTRA_NUMBER, 0);
+        final String text = intent.getStringExtra(MainActivity.EXTRA_TEXT);
+        final int number = intent.getIntExtra(MainActivity.EXTRA_NUMBER, 0);
         TextView ordetOgFejl = (TextView) findViewById(R.id.ordetOgFejl);
 
-        ordetOgFejl.setText("Du skrev ordet "+ " text " +" hvor du mistede "  + number + " liv");
+        ordetOgFejl.setText("Du skrev ordet  "+  text  +" hvor du mistede "  + number + " liv");
+
+        //Preferencemanager til at gemme ordet og antallet af forsøg
+        prefs = PreferenceManager.getDefaultSharedPreferences(this);
+
 
         Button button = (Button) findViewById(R.id.prøvIgenKnapVinder);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                String gemTekst = "";
+                prefs.edit().putString("editText", gemTekst).apply();
                 spilIgen();
         }
     });
